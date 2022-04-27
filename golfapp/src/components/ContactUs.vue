14a943bdb0dc1274b41c3fc18cbb5cb0c7980360
Sejal1109 <sejal.shingal2001@gmail.com>
2022-04-27 13:25:22 -0400

    working on tee times

--- golfapp/src/components/ContactUs.vue
+++ golfapp/src/components/ContactUs.vue
@@ -4,7 +4,13 @@
             <h3> Our Location </h3>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2873.9057354527554!2d-78.90734758427065!3d43.91992564315963!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d51c6c7b07cce1%3A0x99ce6f10c767770e!2sOshawa%20Airport%20Golf%20Club!5e0!3m2!1sen!2sca!4v1650857665835!5m2!1sen!2sca" width="800" height="600"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
-        <div class="column"> </div>
+        <div class="column">
+            <h3> Contact Us </h3>
+                <h5> Oshawa Links Golf Course </h5> 
+                <p> 2400 Ritson Road North <br>
+                    Oshawa, Ontario L1H 8L7 <br>
+                    Phone: (905) xxx-xxxx</p>
+        </div>
     </div>
   </template>
 
--- golfapp/src/components/TeeTimes.vue
+++ golfapp/src/components/TeeTimes.vue
@@ -1,8 +1,10 @@
 <script>
 import '@fullcalendar/core/vdom' // solves problem with Vite
-import FullCalendar from '@fullcalendar/vue3'
+import FullCalendar, { CalendarApi } from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import listPlugin from "@fullcalendar/list";
 
 export default {
   components: {
@@ -10,24 +12,96 @@ export default {
   },
   data() {
     return {
+      title: "",
+      date: "",
+      time: "",
       calendarOptions: {
-        plugins: [ dayGridPlugin, interactionPlugin ],
-        initialView: 'dayGridMonth',
-        dateClick: this.handleDateClick,
+        plugins: [ dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin ],
+        initialView: 'listWeek',
+        displayEventTime: true,
+        eventDisplay: "block",
+  
         events: [
-          { title: 'event 1', date: '2022-04-01' },
+          { title: 'event 1', start: '2022-04-01T16:30:00', end: '2022-04-01T19:30:00'},
           { title: 'event 2', date: '2022-04-02' }
         ]
       }
     }
   },
   methods: {
-    handleDateClick: function(arg) {
-      alert('date click! ' + arg.dateStr)
+    event(){
+      CalendarApi.addEvent(this.title, this.date)
     }
+    
   }
 }
 </script>
 <template>
-  <FullCalendar :options="calendarOptions" />
+  <div id ="container"> 
+    <div class="column left">
+      <FullCalendar :options="calendarOptions" id = "calendar"/>
+    </div>
+    <div class="column right">
+      <form @submit.prevent="event" id ="event">
+      <label for="title"> Event Name: </label>
+      <input type="text" v-model="title" name="title" placeholder="Event Name"/>
+      <label for="Date"> Date: </label>
+      <input type="date" v-model="date" name="date"/>
+      <label for="time"> Time: </label>
+      <input type="time" v-model="time" name="time"/>
+      <button class="btn"> Book </button>
+      </form>
+    </div>
+  </div>
 </template>
+<style scoped>
+  .column {
+    float: left;
+    padding: 10px;
+  }
+  .left {
+    width: 75%;
+  }
+
+  .right {
+    width: 25%;
+  }
+  #calendar{
+    margin: 20px;
+    padding: 10px;
+  }
+  #event{
+    margin-top:50px;
+    margin-right: 30px;
+  }
+  label{
+    display: block;
+    font-weight: bold;
+    margin-top: 10px;
+
+    box-sizing: border-box;
+    text-align: left;
+    margin: auto;
+    width: 100%;
+  }
+  input{
+    padding: 12px 20px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    display: block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    width: 100%;
+  }
+  .btn {
+    font-weight: bold;
+    background-color: #c7d9be;
+    color:  black;
+    padding: 14px 20px;
+    margin-top: 20px;
+    border: none;
+    cursor: pointer;
+    display: block;
+    width: 100%;
+  } 
+</style>
